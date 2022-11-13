7e602f4c7af89c7c649f6afec386643ee8aed241
Dusan Turajlic <dusan@turajlic.fi>
2022-11-13 12:47:47 +0200

    Add lualine

--- lua/dusse/plugins.lua
+++ lua/dusse/plugins.lua
@@ -87,6 +87,12 @@ return packer.startup(function(use)
   -- Git
   use "lewis6991/gitsigns.nvim"
 
+  -- Lualine
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
