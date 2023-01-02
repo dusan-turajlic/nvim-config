ebcc6e870bb7e269477e7327f53ff5025d87660b
Dusan Turajlic <dusan@turajlic.fi>
2023-01-02 08:26:01 +0200

    Changes order to make things a bit better

--- lua/dusse/alpha.lua
+++ lua/dusse/alpha.lua
@@ -17,10 +17,10 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
   dashboard.button("p", "  Open project", ":Telescope projects <CR>"),
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
   dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
   dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
   dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
   dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
